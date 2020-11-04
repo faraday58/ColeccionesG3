6e889896a3aa40ae8d61b60de1015ca5c4830ebb
Armando Rodríguez <jarodriguezvera@gmail.com>
2020-11-04 11:10:38 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace ColeccionesG3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList coleccionDulces = new ArrayList(3);
+
+            coleccionDulces.Add("Borrachitos");
+            coleccionDulces.Add("Bubbulubu");
+            coleccionDulces.Add("Picafresa");
+            coleccionDulces.Add(25.5);
+            foreach(string dulce in coleccionDulces )
+            {
+                Console.WriteLine("Dulce:{0} ", dulce);
+            }
+            coleccionDulces.Clear();
+            Console.WriteLine("Capacidad: {0}", coleccionDulces.Capacity);
+            Console.WriteLine("Contar elementos: {0}", coleccionDulces.Count);
+
+        }
+    }
+}
